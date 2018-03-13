f7af09bbd33837af29abe3ffacd6fc78519ea602
realayi0009 <realayi0009@gmail.com>
2018-03-13 16:34:18 +0800

    js版本

---
+++ README.md
@@ -0,0 +1,4 @@
+# PickStar
+开启你的 Cocos Creator 第一个小游戏吧！
+2018-03-06 stillwater123 COCOS
+http://docs.cocos.com/creator/manual/zh/getting-started/quick-start.html
---
+++ start_project/assets/scripts/Player.js
@@ -0,0 +1,98 @@
+// Learn cc.Class:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/class/index.html
+// Learn Attribute:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/reference/attributes/index.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/life-cycle-callbacks/index.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        jumpHeight: 0,
+        jumpDuration: 0,
+        maxMoveSpeed: 0,
+        accel: 0,
+ 
+        // 跳跃音效资源
+        jumpAudio: {
+            default: null,
+            url: cc.AudioClip
+        },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        this.jumpAction = this.setJumpAction();
+        this.node.runAction(this.jumpAction);
+
+        this.accLeft = false;
+        this.accRight = false;
+
+        this.xSpeed = 0;
+
+        this.setInputControl();
+    },
+
+    start () {
+
+    },
+
+    update (dt) {
+        if (this.accLeft) {
+            this.xSpeed -= this.accel * dt;
+        } else if (this.accRight) {
+            this.xSpeed += this.accel * dt;
+        }
+
+        if (Math.abs(this.xSpeed) > this.maxMoveSpeed ) {
+            this.xSpeed = this.maxMoveSpeed * this.xSpeed / Math.abs(this.xSpeed);
+        }
+
+        this.node.x += this.xSpeed * dt;
+    },
+
+    setJumpAction: function () {
+        var jumpUp = cc.moveBy(this.jumpDuration, cc.p(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
+        var jumpDown = cc.moveBy(this.jumpDuration, cc.p(0, -this.jumpHeight)).easing(cc.easeCubicActionIn());
+        // 添加一个回调函数，用于在动作结束时调用我们定义的其他方法
+        var callback = cc.callFunc(this.playJumpSound, this);
+        return cc.repeatForever(cc.sequence(jumpUp, jumpDown, callback));
+    },
+
+    setInputControl: function () {
+        var self = this;
+        // 添加键盘事件监听
+        // 有按键按下时，判断是否是我们指定的方向控制键，并设置向对应方向加速
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, function (event){
+            switch(event.keyCode) {
+                case cc.KEY.a:
+                    self.accLeft = true;
+                    break;
+                case cc.KEY.d:
+                    self.accRight = true;
+                    break;
+            }
+        });
+
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, function (event){
+            switch(event.keyCode) {
+                case cc.KEY.a:
+                    self.accLeft = false;
+                    break;
+                case cc.KEY.d:
+                    self.accRight = false;
+                    break;
+            }
+        });
+    },
+
+    playJumpSound: function () {
+        // 调用声音引擎播放声音
+        cc.audioEngine.playEffect(this.jumpAudio, false);
+    },
+});
